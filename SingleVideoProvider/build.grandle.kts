a64433a4a59f0e5082306905aa71669cd8b6f721
Zack-Einz <zackeinz14@gmail.com>
2025-08-29 09:44:51 -0400

    Create build.grandle.kts

---
+++ SingleVideoProvider/build.grandle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.lagradost.cloudstream3.gradle") version "1.0.0"
+}
+
+cloudstream {
+    language = "es"
+    description = "Prueba con un solo video"
+    authors = listOf("TU_NOMBRE")
+}
